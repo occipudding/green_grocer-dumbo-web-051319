cd232e812514600f056ab720e003052b67881b8f
Joshua Suskin <jsuskin85@gmail.com>
2019-05-12 02:21:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,6 +16,19 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  coupons.each do |coupon|
+    item_with_coupon = "#{coupon[:item]} W/COUPON"
+    if cart.key?(coupon[:item]) && cart[coupon[:item]][:count] >= coupon[:num]
+      cart[coupon[:item]][:count] -= coupon[:num]
+      if cart.key?(item_with_coupon)
+        cart[item_with_coupon][:count] += 1
+      else
+        cart[item_with_coupon] = cart[coupon[:item]].clone
+        cart[item_with_coupon][:price] = coupon[:cost]
+        cart[item_with_coupon][:count] = 1
+      end
+    end
+  end
   binding.pry
 end
 
@@ -29,5 +42,4 @@ end
 
 #consolidate_cart([{"AVOCADO"=>{:price=>3.0, :clearance=>true}}, {"AVOCADO"=>{:price=>3.0, :clearance=>true}}, {"KALE"=>{:price=>3.0, :clearance=>false}}])
 
-apply_coupons({"CHEESE"=>{:price=>6.5, :clearance=>false, :count=>4},
- "AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>3}}, )
+#apply_coupons({"CHEESE"=>{:price=>6.5, :clearance=>false, :count=>4}, "AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>3}}, [{:item=>"AVOCADO", :num=>2, :cost=>5.0}, {:item=>"CHEESE", :num=>3, :cost=>15.0}])
